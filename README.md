2702ba2cc804c898af5c51a191abbfa82b31a6da
nodeAriful <ariful.node82@gmail.com>
2021-01-01 00:50:19 +0600

    created server

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Stack Bucket Server
 We are creating stack bucket server application
+
+
+## Install Package
+- [x] express
+- [x] cors
+- [x] mongoose
+- [x] nodemon
+- [x] morgan
+
+
+
+## Create Gitignore file
+[toptal](https://www.toptal.com/developers/gitignore)
---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        msg: 'Hello World'
+    })
+})
+
+
+app.listen(8080, ()=>{
+    console.log('Server Listening on port 8080');
+})
+
